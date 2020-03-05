c884517a8b81f368442d542cafd6224579376ded
rnersesian <robert.nersesian@beebryte.com>
2020-03-05 10:55:11 +0100

    first commit

---
+++ route.go
@@ -0,0 +1,15 @@
+package route
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func set_routes(e *echo.Echo) *echo.Echo {
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	return e
+}
---
+++ server.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	
+)
+
+func main() {
+	e := echo.New()
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
